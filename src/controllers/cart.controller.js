da4ffdbfe7b1f226094925c59d99eb440a0ed42f
Harsh Kapil <kapilharsh1999@gmail.com>
2024-06-01 10:06:12 +0530

    -add to cart multiple products functionality upadadted

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,17 +1,29 @@
+import { apiErrorHandler } from "../middlewares/errorhandler.middleware.js";
+
 import Cart from "../models/cart.model.js";
 
 
 /* Add New Product */
 const addToCart = async (req, res, next) => {
-    const { userId, products } = req.body;
+    const { userId, productId, quantity } = req.body;
     if (!userId ) return next(apiErrorHandler(400, "User not found"));
 
-    if (!products) return next(apiErrorHandler(400, "Please Add Products"));
+    if (!productId || !quantity) return next(apiErrorHandler(400, "Please Add Products"));
     
     try {
+        const iscart = await Cart.findOne({ userId });
+        if (iscart) {
+            iscart.products = [...iscart.products, { productId, quantity }];
+            await iscart.save();
+            return res.status(201).json({
+                success: true,
+                data: iscart
+            })
+        }
+        
         const cart = await Cart.create({ 
             userId, 
-            products
+            products: [{ productId, quantity }]
         });
         return res.status(201).json({
             success: true,

9a020ede78b25017740602a8d8b006fa55d450db
Kumar Amrendram <amrendramkumar@gmail.com>
2024-07-19 18:13:50 +0530

    - adds check for product already  in cart

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -14,11 +14,18 @@ const addToCart = async (req, res, next) => {
     /* Check if Cart Already Exists */
     const iscart = await Cart.findOne({ userId });
     if (iscart) {
+      if (iscart.products.find((p) => p.productId.toString() === productId)) {
+        return res.status(201).json({
+          success: true,
+          message: "Product Already Exists in Cart",
+        });
+      }
       iscart.products = [...iscart.products, { productId, quantity }];
       await iscart.save();
       return res.status(201).json({
         success: true,
         data: iscart,
+        message: "Product Added to Cart Successfully",
       });
     }
 
@@ -41,18 +48,38 @@ const addToCart = async (req, res, next) => {
 /* Get Cart Products */
 const getCartProducts = async (req, res, next) => {
   const { userId } = req.query;
-  
-  console.log("userId", req.query);
   if (!userId) return next(apiErrorHandler(400, "UserId is required"));
 
   try {
-    const cart = await Cart.findOne({ userId }).populate("products.productId");
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "products.productId",
+      populate: {
+        path: "image",
+        model: "Image",
+      },
+    });
     if (!cart) return next(apiErrorHandler(404, "No Cart Found"));
+
+    // Transform the products array
+    const transformedProducts = cart.products
+      .map((product) => {
+        if (!product.productId) return null;
+        return {
+          _id: product._id,
+          quantity: product.quantity,
+          ...product.productId._doc,
+          image: product.productId.image.map((img) => img.url),
+        };
+      })
+      .filter((product) => product !== null); // Filter out any null products
 
     return res.status(200).json({
       success: true,
       message: "Cart Products Fetched Successfully",
-      data: cart,
+      data: {
+        ...cart._doc, // Spread other cart properties
+        products: transformedProducts, // Replace products with transformedProducts
+      },
     });
   } catch (error) {
     next(error);

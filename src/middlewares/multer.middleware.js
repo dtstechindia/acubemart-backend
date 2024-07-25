c70cca29c4fbbb7685e7baf62ea2fa5d8c6fc6d1
Kumar Amrendram <amrendramkumar@gmail.com>
2024-07-25 21:11:34 +0530

    - update multer middleware to use /tmp directory

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,26 +1,17 @@
 import multer from "multer";
-import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 
-// Resolve __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// Configure multer to use the /tmp directory
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    const uploadsDir = path.join(__dirname, "../public/images");
-
-    // Create the directory if it doesn't exist
-    if (!fs.existsSync(uploadsDir)) {
-      fs.mkdirSync(uploadsDir, { recursive: true });
-    }
-
-    callback(null, uploadsDir);
+  destination: (req, file, cb) => {
+    // Use /tmp directory for temporary storage
+    cb(null, "/tmp");
   },
   filename: (req, file, cb) => {
+    // Use original file name
     cb(null, file.originalname);
   },
 });
 
+// Initialize multer with the disk storage configuration
 export const upload = multer({ storage: storage });

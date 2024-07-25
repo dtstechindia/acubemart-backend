f2fb591366ae5d665f80acafc672c9a28356601a
Kumar Amrendram <amrendramkumar@gmail.com>
2024-07-25 20:10:01 +0530

    - adds check for public folder

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,26 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Resolve __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "src/public/images");
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
+  destination: (req, file, callback) => {
+    const uploadsDir = path.join(__dirname, "../public/images");
+
+    // Create the directory if it doesn't exist
+    if (!fs.existsSync(uploadsDir)) {
+      fs.mkdirSync(uploadsDir, { recursive: true });
     }
+
+    callback(null, uploadsDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-export const upload = multer({ storage: storage })
+export const upload = multer({ storage: storage });

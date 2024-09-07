9a6a4ba3186a55e31d989c77860c9aa57c9cb90e
Harsh Kapil <kapilharsh1999@gmail.com>
2024-09-07 11:15:48 +0530

    -middleware updated for increasing maximum uploading file size.

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.use(cors({
     credentials: true
   }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static('uploads'));
 
 app.use(errorHandler);
